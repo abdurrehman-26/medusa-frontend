cf9f4348d41aeb2aa325364e5b1df9377d4fc0ac
ar2522006 <ar2522006@gmail.com>
2025-08-17 00:16:06 +0500

    update next config to allow image domains

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      new URL('https://medusa-public-images.s3.eu-west-1.amazonaws.com/**'),
+      new URL('http://localhost:9000/static/**')
+    ],
+  },
+}
+
 export default nextConfig;
