a5d3abcebfb4517469e441024b81d137178949c9
ar2522006 <ar2522006@gmail.com>
2025-08-17 18:11:30 +0500

    refactor Home component to use listProducts for fetching product data

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import { sdk } from "@/lib/sdk";
+import { listProducts } from "@/lib/data/products";
 import ProductCard from "@/modules/Shared/ProductCard";
 import { cookies as nextCookies } from "next/headers";
 
 export default async function Home() {
   const cookies = await nextCookies()
   const region_id = cookies.get("region_id")?.value
-  const productsResponse = await sdk.store.product.list({
-    fields: `*variants.calculated_price`,
-    region_id,
+  const productsResponse = await listProducts({
+    regionId: region_id
   })
   return (
     <div className="font-sans py-10 min-h-screen">
       <main className="flex items-center justify-center flex-wrap gap-10 px-5">
-        {productsResponse.products.map((product) => {
+        {productsResponse.response.products.map((product) => {
           return (
             <ProductCard key={product.id} product={product} />
           )
