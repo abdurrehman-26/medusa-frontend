b9a61904406b0316f74562cf1a74348fb16a7af1
ar2522006 <ar2522006@gmail.com>
2025-08-23 23:36:05 +0500

    remove unused Geist font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import localFont from "next/font/local"
 import "./globals.css";
 import { RegionProvider } from "@/providers/region";
