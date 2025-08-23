a0cd19a73e2fc3604c16830f57f942881fa99113
ar2522006 <ar2522006@gmail.com>
2025-08-23 23:04:41 +0500

    refactor ProductCard component for improved layout and add to cart functionality

--- modules/Shared/ProductCard/index.tsx
+++ modules/Shared/ProductCard/index.tsx
@@ -1,32 +1,44 @@
+"use client";
+import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { formatPrice } from '@/lib/utils'
 import { HttpTypes } from '@medusajs/types'
+import { ShoppingCart } from 'lucide-react';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function ProductCard({product}: {product: HttpTypes.StoreProduct}) {
   const productImage = product.thumbnail ?? product.images?.[0].url ?? "/placeholders/product-placeholder.webp"
+  const addtoCartHandle = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();   // stops the link navigation
+    e.stopPropagation();  // stops bubbling to parent
+    console.log("Added to cart")
+  }
   return (
     <Link href={`/products/${product.handle}`}>
-      <Card className='p-0'>
+      <Card className='p-0 overflow-hidden w-62'>
         <CardContent className='p-0'>
-          <Image
-            alt='product-image' 
-            src={productImage}
-            className='size-60'
-            width={300}
-            height={300} 
-          />
-          <div className='px-1 mt-4'>
-            <div className='p-1'>
-              <span className='truncate font-semibold'>{product.title}</span>
-            </div>
-            <div className='p-1'>
-              <span className="truncate font-bold">
+          <div className='aspect-square overflow-hidden box-content border-b border-foreground/5'>
+            <Image
+              alt='product-image' 
+              src={productImage}
+              width={300}
+              height={300} 
+            />
+          </div>
+          <div className='p-1 px-2 pb-2 mt-1'>
+            <div className='px-1'>
+              <p className='truncate font-medium text-lg mb-2'>{product.title}</p>
+              <p className='truncate text-sm mb-1'>{product.collection?.title || "Unknown Collection"}</p>
+              <p className="truncate text-sm text-foreground/70 mb-2">
                 {formatPrice(product.variants?.[0]?.calculated_price?.calculated_amount ?? 0)}
-              </span>
+              </p>
             </div>
+            <Button type='button' onClick={addtoCartHandle} className='w-full'>
+              <ShoppingCart />
+              Add to cart
+            </Button>
           </div>
         </CardContent>
       </Card>  
