8344a32c9d0184f21f4a52c880febc70e389bb9e
ar2522006 <ar2522006@gmail.com>
2025-08-24 17:01:41 +0500

    add default region for fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import { listProducts } from "@/lib/data/products";
+import { getRegion } from "@/lib/data/regions";
 import ProductCard from "@/modules/Shared/ProductCard";
 import { cookies as nextCookies } from "next/headers";
 
 export default async function Home() {
   const cookies = await nextCookies()
-  const region_id = cookies.get("region_id")?.value
+  const defaultRegion = await getRegion(process.env.NEXT_PUBLIC_MEDUSA_DEFAULT_REGION!)
+  const region_id = cookies.get("region_id")?.value || defaultRegion?.id
   const productsResponse = await listProducts({
     regionId: region_id
   })
   return (
     <div className="font-sans py-10 min-h-screen">
-      <main className="flex items-center justify-center flex-wrap gap-10 px-5">
+      <main className="flex justify-center items-center flex-wrap gap-10 px-5">
         {productsResponse.response.products.map((product) => {
           return (
             <ProductCard key={product.id} product={product} />
