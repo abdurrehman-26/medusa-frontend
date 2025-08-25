940af165db5a6b34e48a508b310b19b878126c08
ar2522006 <ar2522006@gmail.com>
2025-08-25 11:39:50 +0500

    fix: wrap children in main tag for improved layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <RegionProvider>
             <Navbar />
-            {children}
+            <main className="px-2 sm:px-4 md:px-6 mx-auto max-w-7xl min-h-screen">
+              {children}
+            </main>
           </RegionProvider>
         </ThemeProvider>
       </body>
