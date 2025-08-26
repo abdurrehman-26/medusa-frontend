7eeade5c17edecd9ea84a89dd3f7c35b31783b9f
ar2522006 <ar2522006@gmail.com>
2025-08-27 00:24:13 +0500

    feat: add TitledProductRail component for displaying products with a title

---
+++ modules/Home/TitledProductRail/index.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components/ui/button'
+import ProductCard from '@/modules/Shared/ProductCard'
+import { StoreProduct } from '@medusajs/types'
+import React from 'react'
+
+const TitledProductRail = async ({title, products}: {title: string, products: StoreProduct[]}) => {
+  return (
+    <div className='mb-10'>
+      <h1 className='text-4xl font-semibold text-center mb-5'>{title}</h1>
+      <div className='product-grid'>
+        {products.map((product) => {
+            return (
+              <ProductCard key={product.id} product={product} />
+            )
+        })}
+      </div>
+      <div className='pt-5 flex justify-center'>
+        <Button variant='outline' className='rounded-full'>
+            View all
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default TitledProductRail
