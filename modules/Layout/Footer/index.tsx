71e45bc5d7d500d1ad1f753b5911e86a27efc341
ar2522006 <ar2522006@gmail.com>
2025-08-30 00:07:11 +0500

    add responsiveness to footer

--- modules/Layout/Footer/index.tsx
+++ modules/Layout/Footer/index.tsx
@@ -6,13 +6,13 @@ const Footer = async () => {
   const collections = await sdk.store.collection.list().then((collectionResponse) => collectionResponse.collections)
   const categories = await sdk.store.category.list().then((collectionResponse) => collectionResponse.product_categories)
   return (
-    <div className='grid grid-cols-12 max-w-7xl mx-auto p-4 border-t border-border shadow-xs min-h-80'>
-      <div className='col-span-8'>
+    <div className='grid grid-cols-12 max-w-7xl mx-auto p-4 min-h-80 bg-foreground/3'>
+      <div className='col-span-12 sm:col-span-8'>
         <Link href="/" className="text-2xl font-bold text-primary">
           MyStore
         </Link>
       </div>
-      <div className='space-y-2 col-span-2'>
+      <div className='space-y-2 max-sm:pt-5 col-span-12 sm:col-span-2'>
         {collections.map((collection) => {
             return(
                 <div key={collection.id}>
@@ -23,7 +23,7 @@ const Footer = async () => {
             )
         })}
       </div>
-      <div className='space-y-2 col-span-2'>
+      <div className='space-y-2 max-sm:pt-5 col-span-12 sm:col-span-2'>
         {categories.filter((category) => category.metadata?.footer_featured === true).map((category) => {
             return(
                 <div key={category.id}>
