332490312e0c0ac6cec175fb4b13cea4cf9efab2
ar2522006 <ar2522006@gmail.com>
2025-09-03 23:28:11 +0500

    fix the footer css for enhanced look

--- modules/Layout/Footer/index.tsx
+++ modules/Layout/Footer/index.tsx
@@ -6,33 +6,35 @@ const Footer = async () => {
   const collections = await sdk.store.collection.list().then((collectionResponse) => collectionResponse.collections)
   const categories = await sdk.store.category.list().then((collectionResponse) => collectionResponse.product_categories)
   return (
-    <div className='grid grid-cols-12 max-w-7xl mx-auto p-4 min-h-80 bg-foreground/3'>
-      <div className='col-span-12 sm:col-span-8'>
-        <Link href="/" className="text-2xl font-bold text-primary">
-          MyStore
-        </Link>
-      </div>
-      <div className='space-y-2 max-sm:pt-5 col-span-12 sm:col-span-2'>
-        {collections.map((collection) => {
-            return(
-                <div key={collection.id}>
-                <Link href={`/collections/${collection.handle}`}>
-                    {collection.title}
-                </Link>
-                </div>
-            )
-        })}
-      </div>
-      <div className='space-y-2 max-sm:pt-5 col-span-12 sm:col-span-2'>
-        {categories.filter((category) => category.metadata?.footer_featured === true).map((category) => {
-            return(
-                <div key={category.id}>
-                <Link href={`/categories/${category.handle}`}>
-                    {category.name}
-                </Link>
-                </div>
-            )
-        })}
+    <div className='bg-foreground/3'>
+      <div className='grid grid-cols-12 max-w-7xl mx-auto p-4 min-h-80'>
+        <div className='col-span-12 sm:col-span-8'>
+          <Link href="/" className="text-2xl font-bold text-primary">
+            MyStore
+          </Link>
+        </div>
+        <div className='space-y-2 max-sm:pt-5 col-span-12 sm:col-span-2'>
+          {collections.map((collection) => {
+              return(
+                  <div key={collection.id}>
+                  <Link href={`/collections/${collection.handle}`}>
+                      {collection.title}
+                  </Link>
+                  </div>
+              )
+          })}
+        </div>
+        <div className='space-y-2 max-sm:pt-5 col-span-12 sm:col-span-2'>
+          {categories.filter((category) => category.metadata?.footer_featured === true).map((category) => {
+              return(
+                  <div key={category.id}>
+                  <Link href={`/categories/${category.handle}`}>
+                      {category.name}
+                  </Link>
+                  </div>
+              )
+          })}
+        </div>
       </div>
     </div>
   )
