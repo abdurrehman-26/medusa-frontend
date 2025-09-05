d646180ba82619cc3d537645fefec7ed7249b618
ar2522006 <ar2522006@gmail.com>
2025-09-05 15:04:07 +0500

    add promocode functionality

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,20 +3,26 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { sdk } from '@/lib/sdk';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { ArrowRight, Minus, Plus, Trash } from 'lucide-react';
+import { ArrowRight, Minus, Plus, Trash, X } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 import Cookies from 'js-cookie';
-import { StoreCartLineItem } from '@medusajs/types';
-import { setCart } from '@/features/cart/cartSlice';
+import { HttpTypes, StoreCartLineItem } from '@medusajs/types';
+import { addPromoToCart, removePromoFromCart, setCart } from '@/features/cart/cartSlice';
 import { Input } from '@/components/ui/input';
+import { SubmitHandler, useForm} from "react-hook-form"
 
+type PromotionFormValues = {
+  code: string
+}
 const CartPage = () => {
   const cartData = useAppSelector((state) => state.cart.cartData)
   const cartItems = cartData.items
   const cartId = Cookies.get("cartId")
   const dispatch = useAppDispatch()
+
+  const {register, handleSubmit, formState, setError} = useForm<PromotionFormValues>()
   const incrementCartItemQuantity = async (item: StoreCartLineItem) => {
     if (!cartId) {
       return
@@ -119,11 +125,35 @@ const CartPage = () => {
       dispatch(setCart(prevCartItems))
     }
   }
+  const applyPromotion: SubmitHandler<PromotionFormValues> = async (data) => {
+    const {code} = data
+    const applyPromotionResponse = await sdk.client.fetch<{cart: HttpTypes.StoreCart}>(`/store/carts/${cartId}/promotions`, {
+      method: "post",
+      body: {
+        promo_codes: [code]
+      }
+    })
+    if (!applyPromotionResponse.cart.promotions.some((promotion) => promotion.code === code)) {
+      setError("code", {message: "Invalid promo code"})
+    }
+    dispatch(addPromoToCart(applyPromotionResponse.cart.promotions))
+  }
+  const removePromotion = async () => {
+    const removePromotionResponse = await sdk.client.fetch<{cart: HttpTypes.StoreCart}>(`/store/carts/${cartId}/promotions`, {
+      method: "delete",
+      body: {
+        promo_codes: [cartData.promotions[0].code]
+      }
+    })
+    if (removePromotionResponse.cart) {
+      dispatch(removePromoFromCart())
+    }
+  }
   return (
     <div className='py-3'>
       <h1 className='text-3xl font-bold mb-5'>Your Cart</h1>
       <div className='grid grid-cols-12 gap-5'>
-      <div className='space-y-5 flex flex-col col-span-12 md:col-span-8 lg:col-span-9'>
+      <div className={`space-y-5 flex flex-col col-span-12 ${(cartItems && cartItems.length !== 0) && "md:col-span-8 lg:col-span-9"}`}>
         {cartItems && cartItems.length > 0 ? <div className="flex flex-col gap-3 p-2 border border-border rounded-2xl divide-y divide-border">
           {cartItems?.map((cartItem) => (
               <div key={cartItem.id} className='flex py-3'>
@@ -200,11 +230,49 @@ const CartPage = () => {
               </tr>
             </tbody>
           </table>
-          {/* TODO: setup promostions apply */}
-          <form className='flex gap-3'>
-            <Input className='rounded-full' placeholder="Enter promo code" />
-            <Button className='rounded-full' type='button'>Apply</Button>
-          </form>
+          {cartData.promotions && cartData.promotions.length > 0 ? (
+              (() => {
+                const promo = cartData.promotions[0] // only one supported
+                return (
+                  <div>
+                    <p className='text-primary ml-1'>Promo code applied</p>
+                    <div className="flex items-center justify-between rounded-xl border border-gray-200 bg-gray-50 p-1 px-2">
+                      <div className="space-y-1">
+                        <p className="font-semibold text-sm uppercase tracking-wide">
+                          {promo.code}
+                        </p>
+                        {promo.application_method && (
+                          <p className="text-xs text-gray-600">
+                            {promo.application_method.type === "percentage"
+                              ? `${promo.application_method.value}% off`
+                              : `-${promo.application_method.value} ${promo.application_method.currency_code?.toUpperCase()}`}
+                          </p>
+                        )}
+                        {promo.is_automatic && (
+                          <p className="text-xs text-green-600 font-medium">
+                            Applied automatically
+                          </p>
+                        )}
+                      </div>
+                      <button
+                        className="cursor-pointer"
+                        onClick={() => removePromotion()}
+                      >
+                        <X className="h-4 w-4" />
+                      </button>
+                    </div>
+                  </div>
+                )
+              })()
+          ) : <form onSubmit={handleSubmit(applyPromotion)} className='flex gap-3'>
+            <div className='space-y-1'>
+              <Input className='rounded-full' placeholder="Enter promo code" {...register("code")} />
+              <p className='text-sm text-red-600 ml-1'>{formState.errors.code?.message}</p>
+            </div>
+            <Button type="submit" className='rounded-full' disabled={formState.isSubmitting}>
+              {formState.isSubmitting ? "Applying..." : "Apply"}
+            </Button>
+          </form>}
           <Button className='rounded-full items-center'>Go to checkout<ArrowRight /></Button>
         </div>
       )}
