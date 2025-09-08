5ec126283e80f77140ce865ae3cbc64570c6bd47
ar2522006 <ar2522006@gmail.com>
2025-09-09 00:35:16 +0500

    move footer inside redux store provider for accessing state

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,8 +55,8 @@ export default async function RootLayout({
               <main className="px-2 sm:px-4 md:px-6 mx-auto max-w-7xl min-h-screen">
                 {children}
               </main>
+              <Footer />
             </StoreProvider>
-            <Footer />
         </ThemeProvider>
       </body>
     </html>
