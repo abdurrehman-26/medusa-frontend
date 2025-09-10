4e69754161added18105d38fc8f7732b047e97ae
ar2522006 <ar2522006@gmail.com>
2025-09-10 11:09:36 +0500

    add add to cart functionality

--- modules/Shared/ProductCard/index.tsx
+++ modules/Shared/ProductCard/index.tsx
@@ -1,19 +1,38 @@
 "use client";
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
+import { setCart } from '@/features/cart/cartSlice';
+import { sdk } from '@/lib/sdk';
 import { formatPrice } from '@/lib/utils'
-import { useAppSelector } from '@/store/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { HttpTypes } from '@medusajs/types'
 import { ShoppingCart } from 'lucide-react';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { toast } from 'sonner';
 
 function ProductCard({product}: {product: HttpTypes.StoreProduct}) {
+  const cartId = useAppSelector(state => state.cart.cartData.id)
+  const dispatch = useAppDispatch()
   const productImage = product.thumbnail ?? product.images?.[0].url ?? "/placeholders/product-placeholder.webp"
   const addtoCartHandle = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();   // stops the link navigation
     e.stopPropagation();  // stops bubbling to parent
+    if (!cartId || !product.variants?.[0].id) {
+      return
+    }
+
+    sdk.store.cart.createLineItem(cartId, {
+      variant_id: product.variants?.[0].id,
+      quantity: 1,
+    })
+    .then(({cart}) => {
+      if (cart) {
+        dispatch(setCart(cart))
+        toast.success("Product added to cart")
+      }
+    })
   }
   const region = useAppSelector(state => state.region.regionData)
   return (
