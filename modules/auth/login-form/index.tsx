048a251c6737553e0bda02b1c2f46c68f3e98f77
ar2522006 <ar2522006@gmail.com>
2025-09-12 11:37:21 +0500

    add the functionality to login form

--- modules/auth/login-form/index.tsx
+++ modules/auth/login-form/index.tsx
@@ -1,15 +1,59 @@
+"use client"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
+import z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+import { sdk } from "@/lib/sdk"
+import { toast } from "sonner"
+import { useAppDispatch } from "@/store/hooks"
+import { setCustomer } from "@/features/customer/customerSlice"
+
+const schema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters")
+});
+
+type LoginFormValues = z.infer<typeof schema>;
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const dispatch = useAppDispatch()
+  const router = useRouter()
+
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormValues>({
+    resolver: zodResolver(schema), // ✅ Use Zod for validation
+  });
+
+  const handleLogin = async (data: LoginFormValues) => {
+    const {email, password} = data
+
+    try {
+      await sdk.auth.login("customer", "emailpass", {
+        email,
+        password,
+      })
+    } catch {
+      toast.error("Failed to login")
+      return
+    }
+
+    // all next requests will be authenticated
+    const { customer } = await sdk.store.customer.retrieve()
+
+    dispatch(setCustomer(customer))
+
+    router.push("/")
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form onSubmit={handleSubmit(handleLogin)} className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Login to your account</h1>
         <p className="text-muted-foreground text-sm text-balance">
@@ -19,19 +63,23 @@ export function LoginForm({
       <div className="grid gap-6">
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" required />
+          <Input id="email" type="email" {...register("email")} />
+          <p className="text-xs text-red-600">{errors.email?.message}</p>
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
+            <Link
+              href="/forgot-password"
               className="ml-auto text-sm underline-offset-4 hover:underline"
             >
               Forgot your password?
-            </a>
+            </Link>
           </div>
-          <Input id="password" type="password" required />
+          <div>
+            <Input id="password" type="password" {...register("password")} />
+            <p className="text-xs text-red-600">{errors.password?.message}</p>
+          </div>
         </div>
         <Button type="submit" className="w-full">
           Login
